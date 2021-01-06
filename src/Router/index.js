d743885d41eaa0504a72e8f468d5a0b071331d31
System Administrator <root@MacBook-Pro-Konstantin-2.local>
2021-01-06 04:39:58 +0300

    Breadcrumbs block

--- src/Router/index.js
+++ src/Router/index.js
@@ -3,14 +3,19 @@ import IndexPage from "../Pages/Index";
 import CategoryPage from "../Pages/Category";
 import TypePage from "../Pages/Type";
 import DocumentPage from "../Pages/Document";
+import PageContainer from "../Components/PageContainer";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={IndexPage} />
-      <Route path="/categories/:id" exact component={CategoryPage} />
-      <Route path="/types/:id" exact component={TypePage} />
-      <Route path="/documents/:id" exact component={DocumentPage} />
+      <PageContainer>
+        <>
+          <Route path="/" exact component={IndexPage} />
+          <Route path="/categories/:id" exact component={CategoryPage} />
+          <Route path="/types/:id" exact component={TypePage} />
+          <Route path="/documents/:id" exact component={DocumentPage} />
+        </>
+      </PageContainer>
     </Switch>
   </BrowserRouter>
 );
